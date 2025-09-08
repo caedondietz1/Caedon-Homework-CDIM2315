5ef52f4166e25896b8245a65bea4e54582592805
caedondietz1 <cidietz2002@gmail.com>
2025-09-08 11:05:21 -0500

    Add files via upload

---
+++ Homework2-1.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework2_1;
+
+class Program
+{
+    public class GPACalculator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a letter grade:");
+        string grade = Console.ReadLine().ToUpper();
+
+        switch (grade)
+        {
+            case "A":
+                Console.WriteLine("GPA point: 4");
+                break;
+            case "B":
+                Console.WriteLine("GPA point: 3");
+                break;
+            case "C":
+                Console.WriteLine("GPA point: 2");
+                break;
+            case "D":
+                Console.WriteLine("GPA point: 1");
+                break;
+            case "F":
+                Console.WriteLine("GPA point: 0");
+                break;
+            default:
+                Console.WriteLine("Wrong Letter Grade!");
+                break;
+        }
+    }
+}
+}
